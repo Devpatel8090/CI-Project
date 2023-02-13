900a6b5ec5b70f7dad1034486d26152ce9d3d237
Dev Patel <devpatel592001@gmail.com>
2023-02-13 11:40:30 +0530

    forgot , reset , registration html page in MVC UI is created

---
+++ MVC/CI-Platform/CI-Platform/Controllers/AuthenticationController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CI_Platform.Controllers
+{
+    public class AuthenticationController : Controller
+    {
+        public IActionResult login()
+        {
+            return View();
+        }
+
+        public IActionResult forgotPassword()
+        {
+            return View();
+        }
+
+        public IActionResult resetPassword()
+        {
+            return View();
+        }
+
+        public IActionResult registration()
+        {
+            return View();
+        }
+    }
+}
